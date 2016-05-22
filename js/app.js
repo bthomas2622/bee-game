922e560ec69dc1cb7750e7358db072950970ee43
Ben Thomas <bthomas2622@gmail.com>
2016-05-21 19:28:58 -0700

    got new jquery and added test event listener

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ var beeTypes = [
   {
 	species : 'Honey Bee',
 	maxEnergy : 20,
+	pollenCount : 0,
+	honeyCount: 0,
+	beeswaxCount: 0,
+	royalJellyCount: 0,
 	age : 0,
 	speed :  5,
 	stingStrength : 2,
@@ -12,6 +16,10 @@ var beeTypes = [
   {
 	species : 'Carpenter Bee',
 	maxEnergy : 25,
+	pollenCount : 0,
+	honeyCount: 0,
+	beeswaxCount: 0,
+	royalJellyCount: 0,
 	age : 0,
 	speed :  4,
 	stingStrength : 4,
@@ -27,10 +35,14 @@ function createHive(map, foodplaces) {
 	
 }
 
-//foodplacemodel is the knockout observable model object that will dynamically drive my in game statistics
+//beemodel is the knockout observable model object that will dynamically drive my in game statistics
 var beeModel = function(data){
 	this.species = ko.observable(data.species);
 	this.maxEnergy = ko.observable(data.maxEnergy);
+	this.pollenCount = ko.observable(data.pollenCount);
+	this.honeyCount = ko.observable(data.honeyCount);
+	this.beeswaxCount = ko.observable(data.beeswaxCount);
+	this.royalJellyCount = ko.observable(data.royalJellyCount);
 	this.age = ko.observable(data.age);
 	this.speed = ko.observable(data.speed);
 	this.stingStrength = ko.observable(data.stingStrength);
@@ -38,15 +50,21 @@ var beeModel = function(data){
 };
 
 //Viewmodel/controller where data gets accessed from DOM and search
-var ViewModel = function () {
-	//putting this into self where self is viewmodel avoids confusion
+var controller = function () {
+	//putting this into self where self is the controller avoids confusion
 	var self = this;
 	self.bee = ko.observableArray([]);
 	self.bee.push(new beeModel(chosenBee));
+
+	//the listClick function is how I get from the flower field to the statistics
+	self.listClick = function(clickedFlower) {
+		var ageHolder = self.bee()[0].age();
+		var ageHolder = ageHolder + 1;
+		self.bee()[0].age(ageHolder); 
+	};
 };
 
-//apply the knockout observable properties to the ViewModel, essential for dynamic DOM, etc.
-ko.applyBindings(new ViewModel);
-
+//apply the knockout observable properties to the controller, essential for dynamic DOM, etc.
+ko.applyBindings(new controller);
 
 
