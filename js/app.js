9d77d7e98884cd562ea38afe159e7db557de2ca4
Ben Thomas <bthomas2622@gmail.com>
2016-05-30 12:57:49 -0700

    attributing positional data to each flower div object

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-//the beeTypes object contains the information for all of possible bee species that the 
-//model can make observable and the controller and associated functions can manipulate 
+/*the beeTypes object contains the information for all of possible bee species that the 
+model can make observable and the controller and associated functions can manipulate */
 var beeTypes = [
   {
 	species : 'Honey Bee',
@@ -27,7 +27,7 @@ var beeTypes = [
   },
 ];
 
-//bee chosen for adventure, right now just chosen in code
+//bee chosen for adventure, right now just manually chosen in code
 var chosenBee = beeTypes[1];
 
 //beemodel is the knockout observable model object that will dynamically drive my in game statistics
@@ -59,40 +59,60 @@ var controller = function () {
 		createHexHive();
 	};
 
+	//variables holding temporary strings to be replaced in rgb formatting
 	var rdata = '%red%';
 	var gdata = '%green%';
 	var bdata = '%blue%';
-	var formattedrgb; 
+	var xdata = '%xdata%';
+	var ydata = '%ydata%';
+	flowerRGBarray = [];
 
+	//function to create the flowerfield that player bee with traverse
 	function createHexHive() {
-		for(i=0; i<260; i++){
-			var rgb = '<div style="background-color:rgb(%red%,%green%,%blue%);"></div>';
-			var flowerRoller = Math.floor(100*Math.random()).toString();
+		var i, j, r, g, b, rgb, flowerRoller, RGBobject, flower, formattedrgb, flowerRow = [];
+		flowerRGBarray = [];
+		//code to remove previous field if exists to regenerate
+		$("div").remove(".hexagon");
+		//for loop where a single flower (hexagon) is created each loop
+		for(i=0; i<20; i++){
+			//flowerRow is an array that holds the rgb values of each flower in a single row, has to be initialized in each row loop
+			flowerRow = [];
+			for(j=0; j<13; j++){
+				rgb = '<div style="background-color:rgb(%red%,%green%,%blue%);"><div class="flowerdata"></div></div>';
+				flowerRoller = Math.floor(100*Math.random()).toString();
 
-			//this if loop determines the color of the hexagon, determining whether it has pollen or royal jelly
-			if (flowerRoller >= 0 & flowerRoller < 20) {
-				//pollen, should be yellow
-				var r = Math.floor(25*Math.random() + 175).toString();
-				var g = Math.floor(20*Math.random() + 180).toString();
-				var b = Math.floor(25*Math.random() + 100).toString();
+				//random number flowerRoller is the logic determines the color of the hexagon, determining whether it has pollen, royal jelly, or grass
+				if (flowerRoller >= 0 & flowerRoller < 20) {
+					//pollen, should be yellow hue
+					r = Math.floor(25*Math.random() + 175).toString();
+					g = Math.floor(20*Math.random() + 180).toString();
+					b = Math.floor(25*Math.random() + 100).toString();
+				}
+				else if (flowerRoller >= 20 & flowerRoller < 25) {
+					//royal jelly, shades of purple
+					r = Math.floor(55*Math.random() + 200).toString();
+					g = Math.floor(50*Math.random()).toString();
+					b = Math.floor(55*Math.random() + 200).toString();
+				}
+				else {
+					//green grass
+					r = 61;
+					g = 143;
+					b = 61;
+				}
+				var RGBobject = {red:r, green:g, blue:b};
+				flowerRow.push(RGBobject); 
+				var formattedrgb = rgb.replace(rdata, r);
+				var formattedrgb = formattedrgb.replace(gdata, g);  
+				var formattedrgb = formattedrgb.replace(bdata, b);    
+			    var flower = $(formattedrgb).addClass('hexagon');
+			    //pushing flower to the html
+			    $('#flowerfield').append(flower);
 			}
-			else if (flowerRoller >= 20 & flowerRoller < 25) {
-				//royal jelly, shades of purple
-				var r = Math.floor(55*Math.random() + 200).toString();
-				var g = Math.floor(50*Math.random()).toString();
-				var b = Math.floor(55*Math.random() + 200).toString();
-			}
-			else {
-				var r = 61;
-				var g = 143;
-				var b = 61;
-			}
-			var formattedrgb = rgb.replace(rdata, r);
-			var formattedrgb = formattedrgb.replace(gdata, g);  
-			var formattedrgb = formattedrgb.replace(bdata, b);    
-		    var flower = $(formattedrgb).addClass('hexagon');
-		    $('#flowerfield').append(flower);
+			flowerRGBarray.push(flowerRow);
 		}
+		console.log(flowergoodies[0][0])
+		console.log(flowergoodies[1][0])
 	}
 };
 
